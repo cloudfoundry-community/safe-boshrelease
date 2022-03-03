587866091ae3b1f162304d194397668068036c79
Jamie van Dyke <me@fearof.fish>
2022-03-03 16:52:41 +0000

    remove commented out old tests

--- spec/jobs/consul_spec.rb
+++ spec/jobs/consul_spec.rb
@@ -30,49 +30,7 @@ describe 'consul' do
         rendered_template = JSON.parse(template.render(properties, consumes: links))
 
         expect(rendered_template['datacenter']).to eq('vault')
-        # expect(rendered_template).to include('tls_cert_file   = "/var/vcap/jobs/consul/tls/peer/cert.pem"')
-        # expect(rendered_template).to include('tls_key_file    = "/var/vcap/jobs/consul/tls/peer/key.pem"')
-        # expect(rendered_template).to include('address = "0.0.0.0:443"')
-        # expect(rendered_template).to include('ui = false')
-        # expect(rendered_template).to include('api_addr = "https://192.168.0.0:443"')
       end
     end
-
-    # context 'with tls settings' do
-    #   context 'safe.peer.tls.verify = false' do
-    #     it 'skips ssl verification' do
-    #       properties.merge!({
-    #         'safe' => { 
-    #           'peer' => { 
-    #             'tls' => { 
-    #               'verify' => false
-    #             }
-    #           }
-    #         }
-    #       })
-    #       rendered_template = template.render(properties)
-
-    #       expect(rendered_template).to include('tls_skip_verify = "true"')
-    #     end
-    #   end
-
-    #   context 'safe.peer.tls.self_signed = true' do
-    #     it 'does not include cert and key files' do
-    #       properties.merge!({
-    #         'safe' => { 
-    #           'peer' => { 
-    #             'tls' => { 
-    #               'self_signed' => true
-    #             }
-    #           }
-    #         }
-    #       })
-    #       rendered_template = template.render(properties)
-
-    #       expect(rendered_template).not_to include('tls_cert_file   = "/var/vcap/jobs/consul/tls/peer/cert.pem"')
-    #       expect(rendered_template).not_to include('tls_key_file    = "/var/vcap/jobs/consul/tls/peer/key.pem"')
-    #     end
-    #   end
-    # end
   end
 end
